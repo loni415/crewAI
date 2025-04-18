863e79573e785e4fc7c0b9f3b84f5c9b18f3c889
LukasF <lukasfiller@gmail.com>
2025-04-17 21:41:40 -1000

    notes to self

--- update_crewai.sh
+++ update_crewai.sh
@@ -5,3 +5,11 @@
 git fetch upstream
 git merge upstream/main
 git push origin maing
+
+uv tool install crewai
+
+uv tool install crewai --upgrade
+
+#in terminal
+# crewai create crew latest-ai-development
+#cd
